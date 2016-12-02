5a898b356ed90c9b79da67bc44f0561f9c5c0577
Harold <hhausman@gmail.com>
2016-12-02 16:08:16 -0700

    [project] -SNAPSHOT for dev.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thinktopic/gate "0.1.0"
+(defproject thinktopic/gate "0.1.0-SNAPSHOT"
   :description "A library for hacking wepages into your clojure app."
 
   :dependencies [[org.clojure/clojure "1.8.0"]
